a590e8bb4f2ed3726ec6df513627b612ee80483e
2319048236 <111738358+2319048236@users.noreply.github.com>
2022-08-29 23:40:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -70,9 +70,10 @@ def get_memorial_days_count():
 
 # 生日倒计时
 def get_birthday_left():
-  next1 = datetime.strptime(today.year + "-" + datetime.month + "-"+ datetime.day, "%Y-%m-%d")
-  nest2 = datetime.strptime(today.year + "-" + datetime.month + "-"+ datetime.day, "%Y-%m-%d")
-  if next1 < nest2:
+  next1 = datetime.strptime(today.year + "-" + datetime.month + "-" + datetime.day, "%Y-%m-%d")
+  nest2 = datetime.strptime(today.year + "-" + datetime.month + "-" + datetime.day, "%Y-%m-%d")
+  nest3 = 2020
+  if next1 < nest3:
     next = next.replace(year=next.year + 1)
   return (next - nest2).days
 
