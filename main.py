baf797f3191a7693f1ebcbd9c0fe23968f20019c
2319048236 <111738358+2319048236@users.noreply.github.com>
2022-08-29 21:03:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -70,10 +70,7 @@ def get_memorial_days_count():
 
 # 生日倒计时
 def get_birthday_left():
-  if birthday is None:
-    print('没有设置 BIRTHDAY')
-    return 0
-  next = datetime.strptime(str(date.today().year) + "-" + str(date.birthday().year), "%Y-%m-%d")
+  next = datetime.strptime(str(date.today().year) + "-" + datetime, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
