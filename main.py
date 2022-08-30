d17b6e27a27886f55a5ca3f68f453608477bd964
2319048236 <111738358+2319048236@users.noreply.github.com>
2022-08-30 16:56:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ birthday_year = os.getenv('BIRTHDAY_YEAR')
 birthday_month = os.getenv('BIRTHDAY_MONTH')
 birthday_day = os.getenv('BIRTHDAY_day')
 
-birthday1 = LunarDate(birthday_year,birthday_month,birthday_day)#将公立生日转成农历
+#birthday1 = LunarDate(birthday_year,birthday_month,birthday_day)#将公立生日转成农历
 lubaryear1 = today1.year
-lubarmonth1 = birthday1.month
-lubarday1 = birthday1.day
-birthday2 = LunarDate(lubaryear1,lubarmonth1,lubarday1,False)#构建农历日期
+#lubarmonth1 = birthday1.month
+#lubarday1 = birthday1.day
+birthday2 = LunarDate(lubaryear1,birthday_month,birthday_day,False)#构建农历日期
 birthday3 = birthday2.to_solar_date()#把农历转为公历
 
 app_id = os.getenv('APP_ID')
@@ -82,7 +82,6 @@ def get_birthday_left():
   if birthday is None:
     print('没有设置 BIRTHDAY')
     return 0
-  next = datetime.formtimestamp(birthday3)
   #next = datetime.strptime(str(birthday3) , "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
