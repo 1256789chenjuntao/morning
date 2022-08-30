4b9357b75950b4c676a9cb126377d60e79e1533b
2319048236 <111738358+2319048236@users.noreply.github.com>
2022-08-30 17:19:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ birthday_year = os.getenv('BIRTHDAY_YEAR')
 birthday_month = os.getenv('BIRTHDAY_MONTH')
 birthday_day = os.getenv('BIRTHDAY_DAY')
 
-birthday1 = LunarDate(birthday_year,birthday_month,birthday_day)#将公立生日转成农历
+#birthday1 = LunarDate(birthday_year,birthday_month,birthday_day)#将公立生日转成农历
 lubaryear1 = today1.year
-lubarmonth1 = birthday1.month
-lubarday1 = birthday1.day
-birthday2 = LunarDate(lubaryear1,lubarmonth1,lubarday1,False)#构建农历日期
+#lubarmonth1 = birthday1.month
+#lubarday1 = birthday1.day
+birthday2 = LunarDate(lubaryear1,birthday_month,birthday_day,False)#构建农历日期
 birthday3 = birthday2.to_solar_date()#把农历转为公历
 
 app_id = os.getenv('APP_ID')
@@ -85,7 +85,7 @@ def get_birthday_left():
   next = birthday3.to_datetime()
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
-  return (next - today).days
+  return (next - datetime.now()).days
 
 # 彩虹屁 接口不稳定，所以失败的话会重新调用，直到成功
 def get_words():
