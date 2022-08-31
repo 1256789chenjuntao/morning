88b89a23dfd9ef152d5fb66019e189499119312a
2319048236 <111738358+2319048236@users.noreply.github.com>
2022-08-31 11:43:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
 def get_random_color_red():
-  return "0x0c" % randint(0, 0xFFFFFF)
+  return "#%0x0c" % randint(0, 0xFFFFFF)
 
 try:
   client = WeChatClient(app_id, app_secret)
