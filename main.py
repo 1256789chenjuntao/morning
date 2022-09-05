d76295438d010740e7c55a59fa68fcbca746ebcd
2319048236 <111738358+2319048236@users.noreply.github.com>
2022-09-05 08:46:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -352,11 +352,11 @@ data = {
   },
   "v": {
     "value": str(danger1)+"/"+str(danger2),
-    "color": #FF0000
+    "color": "#FF0000"
   },
   "w": {
     "value": alarm2,
-    "color": #FF0000
+    "color": "#FF0000"
   },
   "x": {
     "value": "",
